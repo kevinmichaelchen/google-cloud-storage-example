e3088fa09f1df67241d636ec1a0945fd1f9b1f25
Kevin Chen <kevinmichaelchen@gmail.com>
2018-11-08 12:37:16 -0500

    Successful file upload and directory creation

--- Makefile
+++ Makefile
@@ -3,4 +3,5 @@ all:
 ifndef GOOGLE_APPLICATION_CREDENTIALS
   $(error GOOGLE_APPLICATION_CREDENTIALS is undefined)
 endif
+	echo dog > dog.txt
 	go run .
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"cloud.google.com/go/storage"
 	"context"
+	"io"
 	"log"
 	"os"
 )
@@ -19,10 +20,33 @@ func main() {
 		log.Fatalf("Could not create storage client: %v", err)
 	}
 
-	err = createWithAttrs(client, projectID, "glass-of-water")
+	bucketName := "glass-of-water"
+
+	// CREATE BUCKET
+	//err = createWithAttrs(client, projectID, bucketName)
+	//if err != nil {
+	//	log.Fatalf("Failed to create bucket: %v", err)
+	//}
+
+	ctx = context.Background()
+	r, f := readerFromFile("./dog.txt")
+	defer f.Close()
+	bh := client.Bucket(bucketName)
+	//client.Buckets()
+	_, _, err = upload(bh, ctx, r, "iris/dog.txt", true)
 	if err != nil {
-		log.Fatalf("Failed to create bucket: %v", err)
+		log.Fatalf("Failed to upload to bucket: %v", err)
 	}
+}
+
+func readerFromFile(path string) (io.Reader, *os.File) {
+	var r io.Reader
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r = f
+	return r, f
 }
 
 func createWithAttrs(client *storage.Client, projectID, bucketName string) error {
@@ -34,4 +58,30 @@ func createWithAttrs(client *storage.Client, projectID, bucketName string) error
 		return err
 	}
 	return nil
+}
+
+func upload(bh *storage.BucketHandle, ctx context.Context, r io.Reader, name string, public bool) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	// Next check if the bucket exists
+	if _, err := bh.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	// name must consist entirely of valid UTF-8-encoded runes
+	obj := bh.Object(name)
+	w := obj.NewWriter(ctx)
+	if _, err := io.Copy(w, r); err != nil {
+		return nil, nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	if public {
+		if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+			return nil, nil, err
+		}
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	return obj, attrs, err
 }
